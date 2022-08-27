f08288affe75dcc7ffa8c8cd3f0c7dbe7a22a49f
TranHuyHiep <hiep.tran@codegym.vn>
2022-08-27 15:06:38 +0700

    create file

---
+++ BaiTap01/BaiTap01/SinhVien.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap01
+{
+    class SinhVien
+    {
+        string hoTen;
+        DateTime ngaySinh;
+        double diemLapTrinhTrucQuan;
+        double diemCoSoDuLieu;
+        double diemThietKeWeb;
+
+        public SinhVien()
+        {
+            hoTen = "";
+            ngaySinh = DateTime.Now;
+            diemLapTrinhTrucQuan = 0;
+            diemCoSoDuLieu = 0;
+            diemThietKeWeb = 0;
+        }
+        public void Nhap()
+        {
+            Console.WriteLine("Nhap ho ten: ");
+            hoTen = Console.ReadLine();
+            Console.WriteLine("Nhap ngay sinh: ");
+            ngaySinh = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap diem mon lap trinh: ");
+            diemLapTrinhTrucQuan = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap diem mon co so du lieu: ");
+            diemCoSoDuLieu = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap diem mon thiet ke web: ");
+            diemThietKeWeb = Double.Parse(Console.ReadLine());
+        }
+        // ham xac dinh sinh vien lam khoa luan
+        public bool checkKhoaLuan()
+        {
+            return (((diemLapTrinhTrucQuan + diemCoSoDuLieu + diemThietKeWeb) / 3 >= 8) && (diemCoSoDuLieu >= 5) && (diemThietKeWeb >= 5) && (diemLapTrinhTrucQuan >= 5));
+        }
+        // ham xac dinh sinh vien co lam chuyen de hay khong
+        public bool checkChuyenDeTotNghiep()
+        {
+            return (diemCoSoDuLieu >= 5) && (diemThietKeWeb >= 5) && (diemLapTrinhTrucQuan >= 5);
+        }
+    }
+}
---
+++ BaiTap02/Bai1_155/Bai1_155/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai1_155
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int month = int.Parse(txtMonth.Text);
+            int year = int.Parse(txtYear.Text);
+            lbResult.Text = "Month: " + month + " Year: " + year +
+                " has: " + DateTime.DaysInMonth(year, month) + " date";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtMonth_Enter(object sender, EventArgs e)
+        {
+            TextBox txt = sender as TextBox;
+            YellowChange(txt);
+        }
+
+        private void txtMonth_Leave(object sender, EventArgs e)
+        {
+            TextBox txt = sender as TextBox;
+            WhiteChange(txt);
+        }
+        void YellowChange(TextBox t)
+        {
+            t.BackColor = Color.Yellow;
+        }
+
+        void WhiteChange(TextBox t)
+        {
+            t.BackColor = Color.White;
+        }
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        int count = 0;
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            count++;
+            if (count % 3 == 0) this.BackColor = Color.AliceBlue;
+            else if (count % 3 == 1) this.BackColor = Color.AntiqueWhite;
+            else this.BackColor = Color.Azure;
+        }
+
+        private void txtMonth_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //if ((Convert.ToInt16(e.KeyChar) < Convert.ToInt16('0') ||
+            //    Convert.ToInt16(e.KeyChar) > Convert.ToInt16('9')) &&
+            //    Convert.ToInt16(e.KeyChar) != 8)
+            //{
+            //    MessageBox.Show("enter a digit number, please!");
+            //    e.Handled = true;
+            //}
+            if (!char.IsDigit(e.KeyChar) && Convert.ToInt16(e.KeyChar) != 8)
+            {
+                MessageBox.Show("enter a digit number, please!");
+                e.Handled = true;
+            }
+        }
+
+        private void txtMonth_TextChanged(object sender, EventArgs e)
+        {
+            if(txtMonth.Text.Trim() == "" || txtYear.Text.Trim() == "")
+            {
+                btnResult.Enabled = false;
+            }
+            else
+            {
+                btnResult.Enabled = true;
+
+            }
+        }
+
+        private void toolTip1_Popup(object sender, PopupEventArgs e)
+        {
+
+        }
+    }
+}
