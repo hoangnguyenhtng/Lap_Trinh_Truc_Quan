d45c15de988949bd318bbeb68f8aec98d7d88109
TranHuyHiep <tranhuyhiep0710@gmail.com>
2022-09-04 00:36:40 +0700

    create file

---
+++ Code/BTVN_Buoi1/BTVN_Bai1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTVN_Buoi1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DanhSach danhSach = new DanhSach();
+            danhSach.NhapDSSV();
+            Console.WriteLine("So sinh vien lam khoa luan la: " + danhSach.demSinhVienLamKhoaLuan());
+            Console.WriteLine("So sinh vien lam chuyen de tot nghiep la: " + danhSach.demSinhVienLamChuyenDe());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Code/BTVN_Buoi1/BTVN_Bai2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTVN_Bai2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PhanSo a, b;
+            a = new PhanSo();
+            b = new PhanSo(); ;
+
+            Console.WriteLine("Moi nhap phan so thu nhat: ");
+            a.Nhap();
+
+            Console.WriteLine("Moi nhap phan so thu hai: ");
+            b.Nhap();
+
+
+            Console.WriteLine("Tong hai phan so la: ");
+            Console.WriteLine(a.ToString() + " + " + b.ToString() + " = " + a.Tong(b).ToString());
+
+
+            Console.WriteLine("Hieu hai phan so la: ");
+            Console.WriteLine(a.ToString() + " - " + b.ToString() + " = " + a.Hieu(b).ToString());
+
+            Console.WriteLine("Tich hai phan so la: ");
+            Console.WriteLine(a.ToString() + " * " + b.ToString() + " = " + a.Tich(b).ToString());
+
+            Console.WriteLine("Thuong hai phan so la: ");
+            Console.WriteLine(a.ToString() + " / " + b.ToString() + " = " + a.Thuong(b).ToString());
+            Console.ReadLine();
+        }
+    }
+}
