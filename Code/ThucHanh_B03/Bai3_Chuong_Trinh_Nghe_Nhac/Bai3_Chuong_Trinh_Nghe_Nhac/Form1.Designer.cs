d5f1da92c5af694d230bece907591df81bf8e119
TranHuyHiep <tranhuyhiep0710@gmail.com>
2022-09-29 08:55:02 +0700

    done win media

---
+++ Code/ThucHanh_B03/Bai3_Chuong_Trinh_Nghe_Nhac/Bai3_Chuong_Trinh_Nghe_Nhac/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Bai3_Chuong_Trinh_Nghe_Nhac
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.txtLoiBaiHat = new System.Windows.Forms.RichTextBox();
+            this.cbbThuMuc = new System.Windows.Forms.ComboBox();
+            this.cbbODia = new System.Windows.Forms.ComboBox();
+            this.lbTapTin = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ổ đĩa";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Thư mục";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tập tin";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.axWindowsMediaPlayer1);
+            this.groupBox1.Controls.Add(this.txtLoiBaiHat);
+            this.groupBox1.Controls.Add(this.cbbThuMuc);
+            this.groupBox1.Controls.Add(this.cbbODia);
+            this.groupBox1.Controls.Add(this.lbTapTin);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 536);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(54, 320);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(263, 210);
+            this.axWindowsMediaPlayer1.TabIndex = 9;
+            // 
+            // txtLoiBaiHat
+            // 
+            this.txtLoiBaiHat.Location = new System.Drawing.Point(351, 34);
+            this.txtLoiBaiHat.Name = "txtLoiBaiHat";
+            this.txtLoiBaiHat.Size = new System.Drawing.Size(419, 496);
+            this.txtLoiBaiHat.TabIndex = 8;
+            this.txtLoiBaiHat.Text = "";
+            // 
+            // cbbThuMuc
+            // 
+            this.cbbThuMuc.FormattingEnabled = true;
+            this.cbbThuMuc.Location = new System.Drawing.Point(115, 87);
+            this.cbbThuMuc.Name = "cbbThuMuc";
+            this.cbbThuMuc.Size = new System.Drawing.Size(202, 21);
+            this.cbbThuMuc.TabIndex = 7;
+            this.cbbThuMuc.SelectedIndexChanged += new System.EventHandler(this.cbbThuMuc_SelectedIndexChanged);
+            // 
+            // cbbODia
+            // 
+            this.cbbODia.FormattingEnabled = true;
+            this.cbbODia.Location = new System.Drawing.Point(115, 34);
+            this.cbbODia.Name = "cbbODia";
+            this.cbbODia.Size = new System.Drawing.Size(202, 21);
+            this.cbbODia.TabIndex = 6;
+            this.cbbODia.SelectedIndexChanged += new System.EventHandler(this.cbbODia_SelectedIndexChanged);
+            // 
+            // lbTapTin
+            // 
+            this.lbTapTin.FormattingEnabled = true;
+            this.lbTapTin.Location = new System.Drawing.Point(54, 153);
+            this.lbTapTin.Name = "lbTapTin";
+            this.lbTapTin.Size = new System.Drawing.Size(263, 160);
+            this.lbTapTin.TabIndex = 5;
+            this.lbTapTin.SelectedIndexChanged += new System.EventHandler(this.lbTapTin_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 560);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cbbThuMuc;
+        private System.Windows.Forms.ComboBox cbbODia;
+        private System.Windows.Forms.ListBox lbTapTin;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.RichTextBox txtLoiBaiHat;
+    }
+}
+
---
+++ Code/ThucHanh_B03/Bai3_Chuong_Trinh_Nghe_Nhac/Bai3_Chuong_Trinh_Nghe_Nhac/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai3_Chuong_Trinh_Nghe_Nhac
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            DriveInfo[] drivers = DriveInfo.GetDrives();
+            foreach (DriveInfo d in drivers)
+            {
+                cbbODia.Items.Add(d.Name);
+            }
+        }
+
+        private void cbbODia_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String nameDicrectory = cbbODia.SelectedItem.ToString();
+            DirectoryInfo directory = new DirectoryInfo(nameDicrectory);
+            DirectoryInfo[] directories = directory.GetDirectories("*.*");
+            cbbThuMuc.Items.Clear();
+            foreach (DirectoryInfo d in directories)
+            {
+                cbbThuMuc.Items.Add(d.Name);
+            }
+        }
+
+        private void cbbThuMuc_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String nameDicrectory = cbbODia.SelectedItem.ToString();
+            String nameThuMuc = cbbThuMuc.SelectedItem.ToString();
+            DirectoryInfo directory = new DirectoryInfo(nameDicrectory+ nameThuMuc);
+            lbTapTin.Items.Clear();
+            txtLoiBaiHat.Text = "";
+            FileInfo[] files = directory.GetFiles();
+            foreach(FileInfo d in files)
+            {
+                lbTapTin.Items.Add(d);
+            }
+        }
+
+        private void lbTapTin_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String nameDicrectory = cbbODia.SelectedItem.ToString();
+            String nameThuMuc = cbbThuMuc.SelectedItem.ToString();
+            string nameMusic = lbTapTin.Text.ToString();
+            axWindowsMediaPlayer1.URL = nameDicrectory + "//" + nameThuMuc + "//" + nameMusic;
+
+            FileStream fs = new FileStream("E:\\UTC\\Why-LyricsMCK.txt", FileMode.Open);
+            StreamReader rd = new StreamReader(fs, Encoding.UTF8);
+            String lyric = rd.ReadToEnd();
+            txtLoiBaiHat.Text = lyric;
+        }
+
+
+    }
+}
